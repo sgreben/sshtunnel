079785e5fdf7e881a40959e9246727b955abf61f
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2018-12-19 21:24:36 +0100

    Add package docs

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package sshtunnel lets you dial (and re-publish locally) SSH-tunneled TCP
+// and Unix domain socket connections using the native Go SSH client golang.org/x/crypto/ssh.
+package sshtunnel
---
+++ exec/doc.go
@@ -0,0 +1,3 @@
+// Package sshtunnel lets you dial (and re-publish locally) SSH-tunneled TCP
+// and Unix domain socket connections using external SSH client processes.
+package sshtunnel
